91886c7c1788eb439a1d0c1be3d6af1aa1b36a03
Brendan Meehan <bmmeehan3@gmail.com>
2019-09-22 20:17:49 -0400

    added walletwatch

--- src/project-src.js
+++ src/project-src.js
@@ -2,6 +2,16 @@
 export default {
     list: [
         {
+            id: 'wallet',
+            name: 'WalletWatch',
+            link: 'https://walletwatch.now.sh',
+            repo: 'https://github.com/bmtron/walletwatch',
+            screenshot: 'https://i.gyazo.com/977350cabcf834478abdd7f12dfeef54.png',
+            ssAlt: 'A beautiful screenshot of the main page of my Monty Python quiz app project, also my first ever project!',
+            tech: ['HTML5', 'CSS3', 'React', 'Node.js', 'Express', 'PostgreSQL'],
+            description: 'This handy web app allows users to enter their income and monthly budget items to keep track of their expenses. They can also add in daily single-purchase items and see the impact that purchase has over the course of a week and month. The front-end client was built using React and vanilla CSS styling, and is powered on the back-end by Node, Express, and PostgreSQL to manage the database.'
+        },
+        {
             id: 'wetube',
             name: 'WeTube',
             link: 'https://bmmeehan3-youtube-theater-app.now.sh',
@@ -10,16 +20,6 @@ export default {
             ssAlt: 'A screenshot of the WeTube app in action!',
             tech: ['HTML5', 'CSS3', 'JavaScript', 'React', 'Node.js', 'Express', 'PostgreSQL', 'Socket.io'],
             description: 'This web app allows users to register an account, log in, share their favorite YouTube videos, and chat with each other, all in real time! The front end of this app is powered by React and styled with CSS, while the backend runs on Node.js, Express, and PostgreSQL, relying on Socket.io for real-time interaction.'
-        },
-        {
-            id: 'quiz',
-            name: 'Monty Python\'s Flying Quiz',
-            link: 'https://brendanmeehan.com/quiz/',
-            repo: 'https://github.com/bmtron/QuizApp',
-            screenshot: 'https://i.imgur.com/dRgRaR1.png',
-            ssAlt: 'A beautiful screenshot of the main page of my Monty Python quiz app project, also my first ever project!',
-            tech: ['HTML5', 'CSS3', 'JavaScript', 'jQuery'],
-            description: 'This simple quiz app was created to test a user\'s knowledge of world famous comedy troupe Monty Python. This quiz was the first web app I ever built! This app was built using HTML5, CSS3, JavaScript, and jQuery.'
         },
         {
             id: 'leftovers',
@@ -31,6 +31,5 @@ export default {
             tech: ['HTML5', 'CSS3', 'JavaScript', 'jQuery', 'AJAX'],
             description: 'This helpful food-based application is designed to help users find a new recipe built from the ingredients they already have. Just type in what ingredients you\'d like to use, and the app will generate a full recipe that includes those ingredients. leftOvers also has a randomizer to generate a random recipe for those who don\'t have many ingredients or just want something totally new.'
         }
-        
     ]
 }
